3b5fc9210a2ba588962f62fcc055ffbdff0aaa29
RandyRod <randyro91@gmail.com>
2018-10-20 00:42:44 +0100

    Create service for Person context data retrieval

---
+++ WingsOn.Services/Abstract/IPersonService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WingsOn.Domain;
+
+namespace WingsOn.Services.Abstract
+{
+    public interface IPersonService : IService<Person>
+    {
+        IEnumerable<Person> GetAllMalePassengers();
+    }
+}
---
+++ WingsOn.Services/Concrete/PersonService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WingsOn.Domain;
+using WingsOn.Services.Abstract;
+
+namespace WingsOn.Services.Concrete
+{
+    public class PersonService : ServiceBase, IPersonService
+    {
+        public PersonService()
+        {
+            WingsOnDbContext = new WingsOnDbContext();
+        }
+
+        public Person Get(int id) => WingsOnDbContext.PersonRepository.Get(id);
+
+        public IEnumerable<Person> GetAll() => WingsOnDbContext.PersonRepository.GetAll();
+
+        public IEnumerable<Person> GetAllMalePassengers() => GetAll().Where(p => p.Gender == GenderType.Male);
+    }
+}

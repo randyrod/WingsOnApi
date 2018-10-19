1bc49cbc3979422d440467730c5f9d4765760f84
RandyRod <randyro91@gmail.com>
2018-10-20 00:50:24 +0100

    Create service for Booking data retrieval

---
+++ WingsOn.Services/Abstract/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WingsOn.Domain;
+
+namespace WingsOn.Services.Abstract
+{
+    public interface IBookingService : IService<Booking>
+    {
+        IEnumerable<Person> GetPassengersInFlight(string flightNumber);
+    }
+}
---
+++ WingsOn.Services/Concrete/BookingService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WingsOn.Domain;
+using WingsOn.Services.Abstract;
+
+namespace WingsOn.Services.Concrete
+{
+    public class BookingService : ServiceBase, IBookingService
+    {
+        public BookingService()
+        {
+            WingsOnDbContext = new WingsOnDbContext();
+        }
+
+        public Booking Get(int id) => WingsOnDbContext.BookingRepository.Get(id);
+
+        public IEnumerable<Booking> GetAll() => WingsOnDbContext.BookingRepository.GetAll();
+
+        public IEnumerable<Person> GetPassengersInFlight(string flightNumber)
+        {
+            var passengers = new List<Person>();
+
+            GetAll().Where(b => b.Flight.Number == flightNumber).ToList().ForEach(booking =>
+            {
+                passengers.AddRange(booking.Passengers);
+            });
+
+            return passengers;
+        }
+    }
+}

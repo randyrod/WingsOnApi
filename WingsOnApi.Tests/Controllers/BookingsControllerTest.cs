342a6e19bb0ca5f466fa5221dadd73b6531d4dcf
Randy Rodriguez <randyro91@hotmail.com>
2018-10-20 22:23:47 +0200

    Add basic unit tests for Person and Bookings controllers

---
+++ WingsOnApi.Tests/Controllers/BookingsControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingsOn.Domain;
+using WingsOn.Services.Abstract;
+using WingsOnApi.Controllers;
+using WingsOnApi.Tests.TestUtils;
+
+namespace WingsOnApi.Tests.Controllers
+{
+    [TestClass]
+    public class BookingsControllerTest
+    {
+        private readonly IBookingService _bookingService;
+
+        public BookingsControllerTest()
+        {
+            _bookingService = NinjectHelper.Get<IBookingService>();
+        }
+
+        [TestMethod]
+        public void GetAll()
+        {
+            var controller = new BookingsController(_bookingService);
+
+            var getAllResponse = controller.Get();
+            
+            Assert.IsNotNull(getAllResponse);
+            Assert.IsInstanceOfType(getAllResponse, typeof(IEnumerable<Booking>));
+        }
+        
+        [TestMethod]
+        public void Get()
+        {
+            var controller = new BookingsController(_bookingService);
+
+            var getAllResponse = controller.Get(83);
+            
+            Assert.IsNotNull(getAllResponse);
+            Assert.IsInstanceOfType(getAllResponse, typeof(Booking));
+        }
+
+        [TestMethod]
+        public void GetPassengersInFlight()
+        {
+            var controller = new BookingsController(_bookingService);
+
+            var passengersInFlight = controller.GetPassengersInFlight("PZ696");
+            
+            Assert.IsNotNull(passengersInFlight);
+            Assert.IsInstanceOfType(passengersInFlight, typeof(IEnumerable<Person>));
+        }
+        
+    }
+}
---
+++ WingsOnApi.Tests/Controllers/PersonControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WingsOn.Domain;
+using WingsOn.Services.Abstract;
+using WingsOnApi.Controllers;
+using WingsOnApi.Tests.TestUtils;
+
+namespace WingsOnApi.Tests.Controllers
+{
+    [TestClass]
+    public class PersonControllerTest
+    {
+        private readonly IPersonService _personService;
+
+        public PersonControllerTest()
+        {
+            _personService = NinjectHelper.Get<IPersonService>();
+        }
+        
+        [TestMethod]
+        public void Get()
+        {
+            var controller = new PersonController(_personService);
+
+            var getResponse = controller.Get(91);
+            
+            Assert.IsNotNull(getResponse);
+            
+            Assert.IsInstanceOfType(getResponse, typeof(Person));
+        }
+
+        [TestMethod]
+        public void GetAll()
+        {
+            var controller = new PersonController(_personService);
+
+            var getAllResponse = controller.Get();
+            
+            Assert.IsNotNull(getAllResponse);
+            Assert.IsInstanceOfType(getAllResponse, typeof(IEnumerable<Person>));
+        }
+
+        [TestMethod]
+        public void TestAllMale()
+        {
+            var controller = new PersonController(_personService);
+
+            var maleResponse = controller.GetAllMale();
+            
+            Assert.IsNotNull(maleResponse);
+            Assert.IsInstanceOfType(maleResponse, typeof(IEnumerable<Person>));
+        }
+    }
+}

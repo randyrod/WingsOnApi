f1c91310b967af0a0b8f0e1976ed968c1d6ef09d
Randy Rodriguez <randyro91@hotmail.com>
2018-10-20 22:23:24 +0200

    Change accessor for method to be accessed from test project

--- WingsOnApi/App_Start/Ninject.Web.Common.cs
+++ WingsOnApi/App_Start/Ninject.Web.Common.cs
@@ -41,7 +41,7 @@ namespace WingsOnApi.App_Start
         /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
-        private static IKernel CreateKernel()
+        public static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
             try

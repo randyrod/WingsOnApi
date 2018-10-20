c1688a192379e7ffcc9eff21f86a7eedbb698f7e
RandyRod <randyro91@gmail.com>
2018-10-20 10:33:37 +0100

    Add routing and passengers in booking function

--- WingsOnApi/Controllers/BookingsController.cs
+++ WingsOnApi/Controllers/BookingsController.cs
@@ -1,10 +1,15 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Routing;
+using WingsOn.Domain;
 using WingsOn.Services.Abstract;
 
 namespace WingsOnApi.Controllers
 {
+    [RoutePrefix("wingson/api/booking")]
     public class BookingsController : ApiController
     {
         private readonly IBookingService _bookingService;
@@ -14,18 +19,41 @@ namespace WingsOnApi.Controllers
             _bookingService = bookingService;
         }
 
-        // GET api/<controller>
+        /// <summary>
+        /// Get all bookings
+        /// </summary>
+        /// <returns>List of bookings</returns>
+        [ResponseType(typeof(IEnumerable<Booking>))]
+        [Route("")]
         public IHttpActionResult Get()
         {
             return Content(HttpStatusCode.OK, _bookingService.GetAll(), new JsonMediaTypeFormatter());
         }
 
-        // GET api/<controller>/5
+        /// <summary>
+        /// Get booking by id
+        /// </summary>
+        /// <param name="id">Id of booking</param>
+        /// <returns>Booking</returns>
+        [ResponseType(typeof(Booking))]
+        [Route("{id:int}")]
         public IHttpActionResult Get(int id)
         {
             return Content(HttpStatusCode.OK, _bookingService.Get(id), new JsonMediaTypeFormatter());
         }
 
+        /// <summary>
+        /// Get passengers in a flight
+        /// </summary>
+        /// <param name="flightNumber">Number of flight</param>
+        /// <returns>List of passengers</returns>
+        [ResponseType(typeof(IEnumerable<Person>))]
+        [Route("getpassengersinflight/{flightNumber}")]
+        public IHttpActionResult GetPassengersInFlight(string flightNumber)
+        {
+            return Content(HttpStatusCode.OK, _bookingService.GetPassengersInFlight(flightNumber),
+                new JsonMediaTypeFormatter());
+        }
         // POST api/<controller>
         public void Post([FromBody]string value)
         {

a8cc4acc01f04033ef43486d2b1cb6655be42dba
Randy Rodriguez <randyro91@hotmail.com>
2018-10-20 22:22:51 +0200

    Add Ninject helper to assist the Unit Tests

---
+++ WingsOnApi.Tests/TestUtils/NinjectHelper.cs
@@ -0,0 +1,18 @@
+﻿using Ninject;
+using WingsOnApi.App_Start;
+
+namespace WingsOnApi.Tests.TestUtils
+{
+    internal  static class NinjectHelper
+    {
+        private static readonly IKernel _kernel;
+
+        static NinjectHelper()
+        {
+            _kernel = NinjectWebCommon.CreateKernel();
+        }
+
+        public static T Get<T>() => _kernel.Get<T>();
+
+    }
+}

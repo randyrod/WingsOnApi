96b36c0180d47338498983747e0043bee4037ba2
RandyRod <randyro91@gmail.com>
2018-10-20 10:08:13 +0100

    Add routing and action to get males to person controller

--- WingsOnApi/Controllers/PersonController.cs
+++ WingsOnApi/Controllers/PersonController.cs
@@ -1,10 +1,15 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Description;
+using WingsOn.Domain;
 using WingsOn.Services.Abstract;
+
 
 namespace WingsOnApi.Controllers
 {
+    [RoutePrefix("wingson/api/person")]
     public class PersonController : ApiController
     {
         private readonly IPersonService _personService;
@@ -14,16 +19,39 @@ namespace WingsOnApi.Controllers
             _personService = personService;
         }
 
-        // GET api/<controller>
+        /// <summary>
+        /// Get all people registered
+        /// </summary>
+        /// <returns>A list of people</returns>
+        [ResponseType(typeof(IEnumerable<Person>))]
+        [Route("")]
         public IHttpActionResult Get()
         {
             return Content(HttpStatusCode.OK, _personService.GetAll(), new JsonMediaTypeFormatter());
         }
 
-        // GET api/<controller>/5
+        /// <summary>
+        /// Get a person by Id
+        /// </summary>
+        /// <param name="id">Id of person</param>
+        /// <returns>Person</returns>
+        [ResponseType(typeof(Person))]
+        [Route("{id:int}")]
         public IHttpActionResult Get(int id)
         {
             return Content(HttpStatusCode.OK, _personService.Get(id), new JsonMediaTypeFormatter());
+        }
+
+        /// <summary>
+        /// Get all people that are male
+        /// </summary>
+        /// <returns>List of male people</returns>
+        [ResponseType(typeof(IEnumerable<Person>))]
+        [Route("getmales")]
+        [HttpGet]
+        public IHttpActionResult GetAllMale()
+        {
+            return Content(HttpStatusCode.OK, _personService.GetAllMalePassengers(), new JsonMediaTypeFormatter());
         }
 
         // POST api/<controller>

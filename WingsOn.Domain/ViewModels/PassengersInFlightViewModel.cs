fcfe7cba48467ee23b691182667a95bb9d3cb186
Randy Rodriguez <randyro91@hotmail.com>
2018-11-19 17:17:34 +0100

    Add ViewModel for passenger count in flight

---
+++ WingsOn.Domain/ViewModels/PassengersInFlightViewModel.cs
@@ -0,0 +1,9 @@
+namespace WingsOn.Domain.ViewModels
+{
+    public class PassengersInFlightViewModel
+    {
+        public string FlightNumber { get; set; }
+        
+        public int PassengerAmount { get; set; }
+    }
+}
